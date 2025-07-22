16f41642ef981fa9f5d70fe9d51761f996a0544c
taaa099e <baseballtatsu97@gmail.com>
2025-07-22 22:59:19 +0900

    Add new child registration form with name and birthdate fields

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,4 +4,9 @@ class Child < ApplicationRecord
 
   # 実際に使いたいChildとの関連
   has_many :users, through: :user_children
+  
+  # 子どものプロフィール画像を1枚だけ添付できるようにするActiveStorageの設定
+  has_one_attached :image
 end
+
+

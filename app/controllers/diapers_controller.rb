021aa28462640cafc188962db892075b2b7f2c3b
taaa099e <baseballtatsu97@gmail.com>
2025-08-01 21:42:39 +0900

    Implement diaper record creation: add new/create actions and form with validation

--- app/controllers/diapers_controller.rb
+++ app/controllers/diapers_controller.rb
@@ -9,9 +9,19 @@ class DiapersController < ApplicationController
   end
 
   def new
+    @diaper = current_child.diapers.new(changed_at: Time.current)
   end
 
   def create
+    @diaper = current_child.diapers.new(diaper_params)
+
+    if @diaper.save
+     session.delete(:diaper_changed_at) # セッションから削除
+     redirect_to child_diapers_path(current_child), notice: "授乳記録を保存しました"
+    else
+     flash.now[:alert] = "保存に失敗しました"
+     render :new
+    end
   end
 
   def edit
@@ -21,5 +31,20 @@ class DiapersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  # 許可されたパラメータに変換済みのpee/poopを追加
+  def diaper_params
+    params.require(:diaper).permit(:changed_at, :memo).merge(
+      pee: to_boolean(params[:diaper][:pee]),
+      poop: to_boolean(params[:diaper][:poop])
+    )
+  end
+
+  # 文字列をbooleanに変換
+  def to_boolean(value)
+    ActiveModel::Type::Boolean.new.cast(value)
   end
 end

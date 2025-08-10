4c791ab6c35e53b1d2b4efeb3da58d38e2b02c75
taaa099e <baseballtatsu97@gmail.com>
2025-08-10 22:29:47 +0900

    Generate SleepRecords model and run database migration to create corresponding table

--- app/models/child.rb
+++ app/models/child.rb
@@ -9,6 +9,7 @@ class Child < ApplicationRecord
   has_many :bottles, dependent: :destroy
   has_many :hydrations, dependent: :destroy
   has_many :baby_foods, dependent: :destroy
+  has_many :sleep_records, dependent: :destroy
 
   # 子どものプロフィール画像を1枚だけ添付できるようにするActiveStorageの設定
   has_one_attached :image
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_08_062349) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_10_132638) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -100,6 +100,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_08_062349) do
     t.index ["child_id"], name: "index_hydrations_on_child_id"
   end
 
+  create_table "sleep_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "child_id", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["child_id"], name: "index_sleep_records_on_child_id"
+  end
+
   create_table "user_children", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "child_id", null: false
@@ -129,6 +139,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_08_062349) do
   add_foreign_key "diapers", "children"
   add_foreign_key "feeds", "children"
   add_foreign_key "hydrations", "children"
+  add_foreign_key "sleep_records", "children"
   add_foreign_key "user_children", "children"
   add_foreign_key "user_children", "users"
 end

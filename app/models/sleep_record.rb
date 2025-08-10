4043687a198e6301219b48edf7ca20718f6f3a29
taaa099e <baseballtatsu97@gmail.com>
2025-08-10 23:27:15 +0900

    Add validations for start_time and end_time to SleepRecord model

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,3 +1,18 @@
 class SleepRecord < ApplicationRecord
   belongs_to :child
+
+  validate :validate_start_and_end_time_presence
+
+private
+
+  def validate_start_and_end_time_presence
+    if start_time.blank? && end_time.blank?
+      errors.add(:start_time, "を入力してください")
+      errors.add(:end_time, "を入力してください")
+    elsif start_time.blank? && end_time.present?
+      errors.add(:start_time, "は必須です。end_timeだけの入力はできません。")
+    elsif end_time.present? && end_time < start_time
+      errors.add(:end_time, "は開始時間より後でなければなりません。")
+    end
+  end
 end

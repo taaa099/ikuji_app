3c8db26c9eebdc87e7594a7382b7556b469db83f
taaa099e <baseballtatsu97@gmail.com>
2025-08-11 23:02:34 +0900

    Add new and create actions and view for Bath model

--- app/controllers/baths_controller.rb
+++ app/controllers/baths_controller.rb
@@ -10,9 +10,18 @@ class BathsController < ApplicationController
   end
 
   def new
+    @bath = current_child.baths.new(bathed_at: Time.current)
   end
 
   def create
+    @bath = current_child.baths.new(baths_params)
+    if @bath.save
+      session.delete(:bath_bathed_at) # セッションから削除
+      redirect_to child_baths_path(current_child), notice: "お風呂記録を保存しました"
+    else
+      flash.now[:alert] = "保存に失敗しました"
+      render :new
+    end
   end
 
   def edit

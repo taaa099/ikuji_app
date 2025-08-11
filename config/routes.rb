b792c2dfd5ac7c9b5d5223d718d1f645bdd73a1e
taaa099e <baseballtatsu97@gmail.com>
2025-08-12 07:16:04 +0900

    Add RESTful routes for Vaccinations under children resources

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
    resources :sleep_records
    resources :temperatures
    resources :baths
+   resources :vaccinations
   end
 
   # Defines the root path route ("/")

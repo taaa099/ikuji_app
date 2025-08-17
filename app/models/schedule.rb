b0726afda7ed2a10ee4165f2ee10eb60f1587cbb
taaa099e <baseballtatsu97@gmail.com>
2025-08-18 08:35:01 +0900

    Fix linting errors before PR merge

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,7 +4,7 @@ class Schedule < ApplicationRecord
   validates :start_time, presence: true
   validate :end_time_after_start_time
   validates :title, presence: true, length: { maximum: 50 }
-  validates :all_day, inclusion: { in: [true, false] }
+  validates :all_day, inclusion: { in: [ true, false ] }
   VALID_REPEATS = %w[none daily weekly monthly yearly].freeze
   validates :repeat, inclusion: { in: VALID_REPEATS }
   validates :memo, length: { maximum: 200 }

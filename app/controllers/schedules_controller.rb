a86ace7756b7c5b3708c76433e9d0a5b48fb5227
taaa099e <baseballtatsu97@gmail.com>
2025-08-19 21:58:22 +0900

    Change schedule index ordering to descending

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,7 +3,7 @@ class SchedulesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @schedules = current_child.schedules.order(:start_time)
+    @schedules = current_child.schedules.order(start_time: :desc)
   end
 
   def show

604aac4f63e0242067b23e34341685945afbd5d8
taaa099e <baseballtatsu97@gmail.com>
2025-08-27 14:03:09 +0900

    Add Chart.js bar chart to schedules#index (temporary view)
    - Installed chart.js via yarn and registered it in application.js
    - Added simple bar chart in schedules#index as a placeholder/demo
    - Wrapped chart initialization in DOMContentLoaded check
    - Note: This chart in schedules#index is temporary; actual analysis will be in a dedicated growth analysis view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,25 @@ Rails.start();
 
 import { showNotification } from "./notification_popup";
 import "./notifications"
+
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ctx = document.getElementById("myChart");
+  if (ctx) {
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow"],
+        datasets: [{
+          label: "# of Votes",
+          data: [12, 19, 3],
+          backgroundColor: ["red", "blue", "yellow"]
+        }]
+      },
+      options: {}
+    });
+  }
+});

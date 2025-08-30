e6f779880c9b2832f04916085214975f5e6056d7
taaa099e <baseballtatsu97@gmail.com>
2025-08-31 04:31:43 +0900

    Remove two-factor authentication columns from users and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_29_072700) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_30_193002) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -94,7 +94,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_29_072700) do
     t.bigint "child_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["child_id"], name: "index_diapers_on_child_id"
     t.index ["user_id"], name: "index_diapers_on_user_id"
   end
@@ -166,7 +166,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_29_072700) do
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["child_id"], name: "index_schedules_on_child_id"
     t.index ["user_id"], name: "index_schedules_on_user_id"
   end
@@ -240,16 +240,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_29_072700) do
   add_foreign_key "bottles", "children"
   add_foreign_key "bottles", "users"
   add_foreign_key "diapers", "children"
-  add_foreign_key "diapers", "users"
   add_foreign_key "feeds", "children"
-  add_foreign_key "feeds", "users"
   add_foreign_key "growths", "children"
   add_foreign_key "hydrations", "children"
   add_foreign_key "hydrations", "users"
   add_foreign_key "notifications", "children"
   add_foreign_key "notifications", "users"
   add_foreign_key "schedules", "children"
-  add_foreign_key "schedules", "users"
   add_foreign_key "sleep_records", "children"
   add_foreign_key "sleep_records", "users"
   add_foreign_key "temperatures", "children"

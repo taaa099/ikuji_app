fbbad644cfc07c4a37554b8ac8577a50fec5bf7b
taaa099e <baseballtatsu97@gmail.com>
2025-09-24 05:33:07 +0900

    feat(baths): switch to modal display with Turbo Streams (async)

--- app/controllers/baths_controller.rb
+++ app/controllers/baths_controller.rb
@@ -21,37 +21,81 @@ class BathsController < ApplicationController
 
   def new
     @bath = current_child.baths.new(bathed_at: Time.current)
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "modal",
+          partial: "baths/form_modal",
+          locals: { bath: @bath }
+        )
+      end
+    end
   end
 
   def create
     @bath = current_child.baths.new(baths_params.merge(user: current_user))
-    if @bath.save
-      session.delete(:bath_bathed_at) # セッションから削除
-      redirect_to child_baths_path(current_child), notice: "お風呂記録を保存しました"
-    else
-      flash.now[:alert] = "保存に失敗しました"
-      render :new
+
+    respond_to do |format|
+      if @bath.save
+        format.html { redirect_to child_baths_path(current_child), notice: "お風呂記録を保存しました" }
+        format.turbo_stream
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            "modal",
+            partial: "baths/form_modal",
+            locals: { bath: @bath }
+          )
+        end
+      end
     end
   end
 
   def edit
     @bath = current_child.baths.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "modal",
+          partial: "baths/form_modal",
+          locals: { bath: @bath }
+        )
+      end
+    end
   end
 
   def update
     @bath = current_child.baths.find(params[:id])
-    if @bath.update(baths_params)
-      redirect_to child_baths_path(current_child), notice: "記録を更新しました"
-    else
-      flash.now[:alert] = "更新に失敗しました"
-      render :edit, status: :unprocessable_entity
+
+    respond_to do |format|
+      if @bath.update(baths_params)
+        format.html { redirect_to child_baths_path(current_child), notice: "記録を更新しました" }
+        format.turbo_stream
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            "modal",
+            partial: "baths/form_modal",
+            locals: { bath: @bath }
+          )
+        end
+      end
     end
   end
 
   def destroy
     @bath = current_child.baths.find(params[:id])
-    if @bath.destroy
-      redirect_to child_baths_path(current_child), notice: "お風呂記録を削除しました"
+    @bath.destroy
+
+    respond_to do |format|
+      format.html { redirect_to child_baths_path(current_child), notice: "お風呂記録を削除しました" }
+      format.turbo_stream
     end
   end
 

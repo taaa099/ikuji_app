37c0b554ece42a370e7ed06377d89631417c7989
taaa099e <baseballtatsu97@gmail.com>
2025-12-23 16:12:23 +0900

    Configure Redis usage in production (Action Cable, cache store, Sidekiq) #630

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,11 @@ Rails.application.configure do
   # Replace the default in-process memory cache store with a durable alternative.
   # config.cache_store = :mem_cache_store
 
+  config.cache_store = :redis_cache_store, {
+  url: ENV.fetch("REDIS_URL"),
+  reconnect_attempts: 1
+}
+
   # Replace the default in-process and non-durable queuing backend for Active Job.
   # config.active_job.queue_adapter = :resque
 
